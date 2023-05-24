c7de306576191b54593b18393d700706fdf5c9cc
Juvinvinod <124287134+Juvinvinod@users.noreply.github.com>
2023-05-24 22:46:17 +0530

    initial commit

---
+++ ex1/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','hbs');
+
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+    title: 'View Engine',
+    items: ['Item1','Item2','Item3'],
+})
+});
+
+const port = 3000;
+app.listen(port,()=>{
+    console.log(`Server started at ${port}`);
+})
